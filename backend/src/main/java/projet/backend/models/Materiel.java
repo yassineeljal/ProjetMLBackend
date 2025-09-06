9caa1185121c446c0f849feb25e18191e7ddf699
yassineeljal <yassineeljal0@gmail.com>
2025-09-06 13:04:56 -0400

    Ajout des relation entre model et des variable

--- backend/src/main/java/projet/backend/models/Materiel.java
+++ backend/src/main/java/projet/backend/models/Materiel.java
@@ -1,5 +1,24 @@
 package projet.backend.models;
 
+import jakarta.persistence.*;
 
+import java.util.Date;
+
+@Entity
 public class Materiel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double weight;
+    private String material_name;
+    private Date location_date;
+    private boolean isRented;
+    private String category;
+    @ManyToOne
+    private People people;
+
+    public Materiel() {
+
+    }
+
 }
--- backend/src/main/java/projet/backend/models/People.java
+++ backend/src/main/java/projet/backend/models/People.java
@@ -1,4 +1,16 @@
 package projet.backend.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class People {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private String gender;
 }
