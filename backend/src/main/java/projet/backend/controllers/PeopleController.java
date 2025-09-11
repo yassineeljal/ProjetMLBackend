7749e6278cb37537710caf1d91527607df898c7f
yassineeljal <yassineeljal0@gmail.com>
2025-09-11 12:55:43 -0400

    ajou de data.sql

--- backend/src/main/java/projet/backend/controllers/PeopleController.java
+++ backend/src/main/java/projet/backend/controllers/PeopleController.java
@@ -1,4 +1,23 @@
 package projet.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import projet.backend.models.People;
+import projet.backend.services.PeopleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
+@CrossOrigin
 public class PeopleController {
+
+    @Autowired
+    PeopleService peopleService;
+
+    @PostMapping("/findUser/{name}")
+    public List<People> findUser(@PathVariable String name){
+        return peopleService.getPeopleByName(name);
+    };
+
 }
