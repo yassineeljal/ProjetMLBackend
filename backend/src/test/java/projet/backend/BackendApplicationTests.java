5a71a43e194f932a35d69d865f75db66c7fce3ca
root <root@logiciel-1-eljalyas.10.10.0.5>
2025-10-16 20:31:02 +0000

    test

--- backend/src/test/java/projet/backend/BackendApplicationTests.java
+++ backend/src/test/java/projet/backend/BackendApplicationTests.java
@@ -3,11 +3,16 @@ package projet.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=none"
+})
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
-
     }
 }
