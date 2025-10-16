73371b1e7aafedc460f085f671a3984e81d81da4
root <root@logiciel-1-eljalyas.10.10.0.5>
2025-10-16 20:25:59 +0000

    test

--- backend/src/test/java/projet/backend/BackendApplicationTests.java
+++ backend/src/test/java/projet/backend/BackendApplicationTests.java
@@ -2,6 +2,8 @@ package projet.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
